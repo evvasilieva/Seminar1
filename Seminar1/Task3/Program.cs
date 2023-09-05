07b31d4c47960af4704378c133795d64944876fa
vectorpy <vectorpy@mail.ru>
2023-09-05 15:32:02 +0300

    Task3

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6 (Task3): Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int chet = num % 2;
+if (chet == 0)
+{
+    Console.WriteLine("Число " + " " + num + " " + " четное?");
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("Число " + " " + num + " " + " четное?");
+    Console.WriteLine("Нет!");
+}
