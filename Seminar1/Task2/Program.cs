3eab6558a6f124f2885b8357509edba01b0b81e2
vectorpy <vectorpy@mail.ru>
2023-09-05 15:15:59 +0300

    Task2

---
+++ Seminar1/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4 (Task 2): Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+﻿Console.WriteLine("Введите три числа, и узнайте, какое из них имеет максимальное значение.");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int maxNum = 0;
+if (num1 > maxNum) maxNum = num1;
+if (num2 > maxNum) maxNum = num2;
+if (num3 > maxNum) maxNum = num3;
+Console.Write("Максимальное значение имеет число " + " " + maxNum);
